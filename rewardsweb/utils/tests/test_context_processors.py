8a5eb99cca072bf4a60674387f95aa539e4bbbec
Ivica Paleka <ipaleka@asastats.com>
2025-12-07 13:40:12 +0100

    Added pagination template snippet

--- rewardsweb/utils/tests/test_context_processors.py
+++ rewardsweb/utils/tests/test_context_processors.py
@@ -1,8 +1,9 @@
 """Testing module for :py:mod:`utils.context_processors` module."""
 
 from django.conf import settings
+from django.http import QueryDict
 
-from utils.context_processors import global_constants
+from utils.context_processors import global_constants, pagination_context
 
 
 class TestUtilsContextProcessorsFunctions:
@@ -10,11 +11,26 @@ class TestUtilsContextProcessorsFunctions:
 
     # # global_constants
     def test_utils_context_processors_global_constants_functionality(self, mocker):
-
         returned = global_constants(mocker.MagicMock())
         assert returned == {
             "PROJECT_OWNER": settings.PROJECT_OWNER,
             "PROJECT_NAME": f"{settings.PROJECT_OWNER} Rewards",
             "PROJECT_WEBSITE_NAME": f"{settings.PROJECT_OWNER} Rewards website",
             "PROJECT_DOMAIN": settings.PROJECT_DOMAIN,
+        }
+
+    # # pagination_context
+    def test_utils_context_processors_pagination_context_for_no_query(self, mocker):
+        request = mocker.MagicMock()
+        request.GET = QueryDict("page=1")
+        returned = pagination_context(request)
+        assert returned == {"query_string": "", "has_query_params": False}
+
+    def test_utils_context_processors_pagination_context_functionality(self, mocker):
+        request = mocker.MagicMock()
+        request.GET = QueryDict("foo=bar&foobar=1&page=10")
+        returned = pagination_context(request)
+        assert returned == {
+            "query_string": "foo=bar&foobar=1",
+            "has_query_params": True,
         }

f6f63a9dd0b897eb450fc12a5c30cbd4b5efc9ec
Ivica Paleka <ipaleka@asastats.com>
2026-01-08 18:52:46 +0100

    Issue assigning wrapped as sync_to_async call

--- rewardsweb/api/views.py
+++ rewardsweb/api/views.py
@@ -369,9 +369,11 @@ class AddIssueView(LocalhostAPIView):
         if contribution_data:
             data, errors = await process_issue(request.data)
             if data:
-                Contribution.objects.assign_issue(
-                    data.get("id"), contribution_data.get("id")
-                )
+                await sync_to_async(
+                    lambda: Contribution.objects.assign_issue(
+                        data.get("id"), contribution_data.get("id")
+                    )
+                )()
                 return Response(data, status=status.HTTP_201_CREATED)
 
         return Response(errors, status=status.HTTP_400_BAD_REQUEST)
